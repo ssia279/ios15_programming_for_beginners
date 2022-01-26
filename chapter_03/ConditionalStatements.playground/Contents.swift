66d0043060f44630697be429be8ad7053dc9ac20
Johnson Wang <freelance@johnsonwang.com>
2022-01-26 14:28:54 -0800

    complete chapter 3 exercises.

--- chapter_03/ConditionalStatements.playground/Contents.swift
+++ chapter_03/ConditionalStatements.playground/Contents.swift
@@ -17,3 +17,34 @@ if customerAge < drinkingAgeLimit {
 } else {
   print("Over age limit.")
 }
+
+var trafficLightColor = "Yellow"
+if trafficLightColor == "Red" {
+  print("Stop")
+} else if trafficLightColor == "Yellow" {
+  print("Caution")
+} else if trafficLightColor == "Green" {
+  print("Go")
+} else {
+  print("Invalid color")
+}
+
+trafficLightColor = "Green"
+switch trafficLightColor {
+case "Red":
+  print("Stop")
+case "Yellow":
+  print("Caution")
+case "Green":
+  print("Go!")
+default:
+  print("Invalid Color")
+}
+
+var spouseName: String?
+spouseName = nil
+print(spouseName ?? "No value in spouseName")
+if let spouseName = spouseName {
+  let greeting = "Hello, " + spouseName
+  print(greeting)
+}
