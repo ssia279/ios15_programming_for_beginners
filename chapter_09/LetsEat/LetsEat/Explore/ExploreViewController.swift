98afd749cd86daec3dd3d63e9a56cdd61193c0ac
Johnson Wang <freelance@johnsonwang.com>
2022-02-01 11:22:20 -0800

    - added icons to the project - create exploreViewController file. - added collection view to explore view. - create outlet for collectionview.

---
+++ chapter_09/LetsEat/LetsEat/Explore/ExploreViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreViewController.swift
+//  LetsEat
+//
+//  Created by Johnson Wang on 2/1/22.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+  @IBOutlet var collectionView: UICollectionView!
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
